7c102e07dbffdb51942cf7beef7693e3ba93e55d
epearson-nmdp <epearson@nmdp.org>
2015-03-10 17:02:16 -0500

    add script to push allele codes to docker container volume

---
+++ service/src/main/scripts/update_allele_codes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+REMOTE_USER=epearson
+REMOTE_HOST=epearsonone
+ALLELE_CODE_URL=https://bioinformatics.bethematchclinical.org/HLA/alpha.v3.zip
+REMOTE_DIR=$(ssh $REMOTE_USER@$REMOTE_HOST "docker inspect epitope-service | jq \".[].Volumes[\\\"/var/lib/epitope-service\\\"]\"")
+curl $ALLELE_CODE_URL | ssh $REMOTE_USER@$REMOTE_HOST "sudo sh -c \"cat - >$REMOTE_DIR/alpha.v3.zip\""
